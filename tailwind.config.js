131e746c1c999b4dd0bff85492e3894e4ed20881
Tom Tiedemann <tom.tiedemann@gmail.com>
2023-03-21 14:39:14 -0400

    moved the files to a different directory

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+
+    fontFamily: {
+
+      serif: ['Times New Roman', 'serif'],
+
+    },
+
+    extend: {
+
+      
+
+      colors: {
+
+        'primary': '#dfeefc'
+
+      }
+    },
+  },
+  plugins: [
+
+     
+
+  ],
+}
